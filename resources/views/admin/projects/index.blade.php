7da7cfb82f20c31e04d675bab5cb01e2084258df
SimoneNegrisoli <simo.negrisoli@gmail.com>
2024-01-12 22:39:28 +0100

    template index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,16 +1,15 @@
 @extends('layouts.app')
 @section('content')
     <section class="container">
-        <div class="d-flex justify-content-between ">
+        <div class="d-flex justify-content-between align-items-center  pt-4">
             <h1>Projects</h1>
             <a href="{{ route('admin.projects.create') }}" class="btn btn-success ">Aggiungi nuovo progetto</a>
         </div>
-        <table class="table">
+        <table class="table table-striped mt-4">
             <tbody>
-
                 @foreach ($projects as $key => $project)
                     <tr>
-                        <td>{{ $project->title }}</td>
+                        <td class="align-middle">{{ $project->title }}</td>
                         <td class="d-flex justify-content-end align-items-center">
                             <a href="{{ route('admin.projects.show', $project->id) }}" class="btn btn-success mx-2">Mostra</a>
 
